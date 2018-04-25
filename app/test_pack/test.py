f1e74cfbbb206b573ec4a106c659dbf69d66cdd4
aedotpy <aedotpy@gmail.com>
2018-04-25 15:00:14 +0800

    add new feature [UO]

--- app/test_pack/test.py
+++ app/test_pack/test.py
@@ -50,3 +50,40 @@ import matplotlib.pyplot as plt
 # plt.xlabel('time')
 # plt.ylabel('price')
 # plt.show()
+
+'''
+ * 终极摆动指标
+ * http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:ultimate_oscillator
+
+ * BP = Close - Minimum(Low or Prior Close).
+
+ * TR = Maximum(High or Prior Close)  -  Minimum(Low or Prior Close)
+
+ * Average7 = (7-period BP Sum) / (7-period TR Sum)
+ * Average14 = (14-period BP Sum) / (14-period TR Sum)
+ * Average28 = (28-period BP Sum) / (28-period TR Sum)
+
+ * UO = 100 x [(4 x Average7)+(2 x Average14)+Average28]/(4+2+1)
+
+'''
+
+
+# df = ts.get_hist_data('600179', start='2018-01-01')
+# Close = df['close'].head(1).values[0]
+# Minimum = df['low'].head(1).values[0]
+# Maximum = df['high'].head(1).values[0]
+# BP = Close - Minimum
+# TR = Maximum - Minimum
+#
+#
+# def get_x_period_av(data, x):
+#     return sum(data['close'][:x].values - data['low'][:x].values) / sum(data['high'][:x].values - data['low'][:x].values)
+#
+#
+# av_7 = get_x_period_av(df, 7)
+# av_14 = get_x_period_av(df, 14)
+# av_28 = get_x_period_av(df, 28)
+# 
+# UO_value = 100 * ((4 * av_7) + (2 * av_14) + av_28)/(4+2+1)
+#
+# print(UO_value)
