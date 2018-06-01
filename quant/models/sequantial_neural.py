a3913c006cae9cbd55af50b740981b508fde7dda
greg <czchen228@gmail.com>
2018-06-01 16:36:06 +0800

    完善ann模型

--- quant/models/sequantial_neural.py
+++ quant/models/sequantial_neural.py
@@ -6,20 +6,29 @@ from keras import Sequential
 from keras.layers import Dense, Dropout
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
-
-from quant.dao.k_data_dao import k_data_dao
-from quant.feature_utils.feature_collector import collect_features
+from quant.log.quant_logging import quant_logging as logging
+from quant.models.pca_model import PCAModel
 from quant.models.base_model import BaseModel
+from quant.dao.k_data_model_log_dao import k_data_model_log_dao
+from sklearn.externals import joblib
+import os
 
 
 class SequantialNeural(BaseModel):
+    model_name = 'sequantial_neural'
 
     def training_model(self, code, data, features):
-        data = k_data_dao.get_k_data(code, '2015-01-01', datetime.now().strftime("%Y-%m-%d"))
+        X = data[features]
+        y = data['next_direction']
 
-        data, features = collect_features(data)
+        # normalization
+        X = preprocessing.scale(X)
 
-        X_train, x_test, Y_train, y_test = train_test_split(data[features], data['next_direction'], test_size=.3)
+        # pca缩放
+        pca = PCAModel().load(code)
+        X = pca.transform(X)
+
+        X_train, x_test, y_train, y_test = train_test_split(X, y, test_size=.3)
 
         # normalization
         X_train = preprocessing.scale(X_train)
@@ -33,27 +42,38 @@ class SequantialNeural(BaseModel):
         sequantial_model.add(Dropout(0.5))
         sequantial_model.add(Dense(128, activation='relu'))
         sequantial_model.add(Dropout(0.5))
-        # sequantial_model.add(Dense(64, activation='relu'))
-        # sequantial_model.add(Dropout(0.5))
-        # sequantial_model.add(Dense(16, activation='relu'))
-        # sequantial_model.add(Dropout(0.5))
+
         sequantial_model.add(Dense(1, activation='tanh'))
         sequantial_model.compile(optimizer='sgd', loss='binary_crossentropy', metrics=['accuracy'])
 
-        # sequantial_model.summary()
-
         # traning performance
-        sequantial_model.fit(X_train, Y_train, epochs=10, batch_size=128)
+        sequantial_model.fit(X_train, y_train, epochs=10, batch_size=128)
+        train_model_score = sequantial_model.evaluate(X_train, y_train, batch_size=128)
 
         # test performance
         test_model_score = sequantial_model.evaluate(x_test, y_test, batch_size=128)
-        print('test model score: %s' % test_model_score)
-
-        # whole data performance
-        # sequantial_model.fit(data[features], data['next_direction'])
+        logging.logger.debug('test model score: %s' % test_model_score)
 
         full_model_score = sequantial_model.evaluate(data[features], data['next_direction'])
-        print('full model score: %s' % full_model_score)
+        logging.logger.debug('full model score: %s' % full_model_score)
 
-        # summary
-        sequantial_model.summary()
+        # 记录日志
+        k_data_model_log_dao.insert(code=code, name=self.model_name
+                                    , best_estimator=sequantial_model.to_json(),
+                                    train_score=train_model_score, test_score=test_model_score
+                                    , desc="full_model_score:%s" % full_model_score)
+        # 输出模型
+        joblib.dump(sequantial_model, self.get_model_path(code, self.model_name))
+
+    def predict(self, code, data):
+        model_path = self.get_model_path(code, self.model_name)
+
+        if not os.path.exists(model_path):
+            logging.logger.error('model not found, code is %s:' % code)
+            return
+
+        sequantial_model = joblib.load(model_path)
+
+        y_pred = sequantial_model.predict(data)
+
+        return y_pred
