fa82fedf92d102d67f350a5dad8f6ba7763fc8c0
greg <czchen228@gmail.com>
2018-06-03 00:13:53 +0800

    集成训练模型

--- quant/models/models_manage.py
+++ quant/models/models_manage.py
@@ -34,9 +34,9 @@ def init_db():
 if __name__ == '__main__':
     init_db()
 
-    #k_data_manage.predict_k_data()
+    k_data_manage.predict_k_data()
 
-    schedule.every().day.at("17:00").do(k_data_manage.training_k_data())
+    #schedule.every().day.at("17:00").do(k_data_manage.training_k_data())
 
     '''
     while True:
