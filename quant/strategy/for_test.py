6f9c93b1c7650d791426a5cb84636011ef2ee511
aedotpy <aedotpy@gmail.com>
2018-06-02 23:03:53 +0800

    stock_code_set(stock_code_tuple)

--- quant/strategy/for_test.py
+++ quant/strategy/for_test.py
@@ -3,23 +3,24 @@ import tushare as ts
 
 from quant.common_tools.decorators import exc_time
 
-
 hs_300 = ts.get_hs300s()
 code_list = list(hs_300['code'])
 
-pair_list = []
+
+
 
 @exc_time
 def for_test_test():
-
+    temp_set = set()
     for i in range(0, len(code_list)):
         for j in range(1, len(code_list)):
-            pair_list.append((code_list[i], code_list[j]))
+            code1 = code_list[i]
+            code2 = code_list[j]
+            code_tuple = sorted((code1, code2))
+            temp_set.add((code_tuple[0], code_tuple[1]))
 
-    print(pair_list)
-
+    print(len(temp_set))
 
 
 if __name__ == '__main__':
-
     for_test_test()
