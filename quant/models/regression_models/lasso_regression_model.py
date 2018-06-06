a11e081a2b44d6312c6286c043af3f9855fe670e
aedotpy <aedotpy@gmail.com>
2018-06-06 14:47:17 +0800

    lasso model

--- quant/models/regression_models/lasso_regression_model.py
+++ quant/models/regression_models/lasso_regression_model.py
@@ -1,9 +1,13 @@
 # coding = utf-8
 # ae_h - 2018/6/5
+import os
+from sklearn.externals import joblib
 from sklearn.model_selection import train_test_split, cross_val_score
 
+from quant.common_tools.decorators import exc_time
+from quant.dao.k_data.k_data_model_log_dao import k_data_model_log_dao
 from quant.models.base_model import BaseModel
-from sklearn import linear_model, preprocessing
+from sklearn import linear_model, preprocessing, metrics
 from sklearn.metrics import accuracy_score
 from quant.log.quant_logging import logger
 from quant.models.k_data import MODULE_NAME
@@ -16,7 +20,7 @@ class LassoRegressionModel(BaseModel):
 
     def training_model(self, code, data, features, *args):
         X = data[features]
-        y = data[args]
+        y = data['close']
 
         # normalization
         X = preprocessing.scale(X)
@@ -25,25 +29,47 @@ class LassoRegressionModel(BaseModel):
         pca = PCAModel(self.module_name).load(code)
         X = pca.transform(X)
 
-        X_train, x_test, y_train, y_test = train_test_split(X, y, test_size=.3, shuffle=False)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3, shuffle=False)
 
-        lasson_model = linear_model.LassoCV(n_jobs=-1, random_state=10, normalize=True)
-
-        logger.debug(lasson_model)
+        lasson_model = linear_model.LassoCV(alphas=[10, 1, 0.5, 0.25, 0.1, 0.005, 0.0025, 0.001], n_jobs=-1,
+                                            random_state=10)
 
         lasson_model.fit(X_train, y_train)
 
-        y_pred = cross_val_score(lasson_model, x_test)
+        test_score = lasson_model.score(X_test, y_test)
 
-        test_score = accuracy_score(y_test, y_pred)
+        y_pred = lasson_model.predict(X_test)
+        mse = metrics.mean_squared_error(y_test, y_pred)
 
-        logger.debug('test score %s ' %test_score)
+        mse = '%.4e' % mse
 
+        logger.debug('mse: %s' % metrics.mean_squared_error(y_test, y_pred))
+
+        # full data training
         lasson_model.fit(X, y)
 
-        full_set_score = lasson_model.score(X, y)
+        # 记录日志
+        k_data_model_log_dao.insert(code=code, name=self.model_name
+                                    , best_estimator=lasson_model,
+                                    train_score=test_score, test_score=mse)
 
-        logger.debug('full set score %s' % full_set_score)
+        # 输出模型
+        joblib.dump(lasson_model, self.get_model_path(code, self.module_name, self.model_name))
 
+    @exc_time
     def predict(self, code, data):
-        pass
+        model_path = self.get_model_path(code, self.module_name, self.model_name)
+
+        if not os.path.exists(model_path):
+            logger.error('model not found, code is %s:' % code)
+            return
+
+        X = preprocessing.scale(data)
+        pac = PCAModel(self.module_name).load(code)
+        X = pac.transform(X)
+
+        lasso_regression_model = joblib.load(model_path)
+
+        y_pred = lasso_regression_model.predict(X)
+
+        return int(y_pred[0])
---
+++ quant/test/models/regression_models/lasso_regression_model_test.py
@@ -0,0 +1,27 @@
+# ae_h - 2018/6/6
+import unittest
+from datetime import datetime
+
+from quant.dao.k_data_60m.k_data_60m_dao import k_data_60m_dao
+from quant.models.regression_models.lasso_regression_model import LassoRegressionModel
+from quant.log.quant_logging import logger
+from quant.models.pca_model import PCAModel
+from quant.test import before_run
+
+
+class Linear_Regression_Test(unittest.TestCase):
+    def setUp(self):
+        before_run()
+
+    def test_training(self):
+        code = '600196'
+        data, features = k_data_60m_dao.get_k_data_with_features(code, '2015-01-01',
+                                                             datetime.now().strftime("%Y-%m-%d"))
+
+        logger.debug("features:%s, length:%s" % (features, len(features)))
+
+        pac = PCAModel('k_data')
+        pac.training_model(code=code, data=data, features=features)
+
+        model = LassoRegressionModel()
+        model.training_model(code, data, features)
