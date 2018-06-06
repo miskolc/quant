bab57bfc0e933d0cc5c873751f63b6780df1bb02
aedotpy <aedotpy@gmail.com>
2018-06-06 13:23:18 +0800

    no message

--- quant/models/regression_models/linear_regression_model.py
+++ quant/models/regression_models/linear_regression_model.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split, cross_val_score
 
 from quant.models.base_model import BaseModel
 from sklearn import linear_model, preprocessing
-from sklearn.metrics import accuracy_score
+from sklearn import metrics
 from quant.log.quant_logging import logger
 from quant.models.k_data import MODULE_NAME
 from quant.models.pca_model import PCAModel
@@ -15,8 +15,10 @@ class LinearRegressionModel(BaseModel):
     model_name = "linear_regression_model"
 
     def training_model(self, code, data, features, *args):
+
         X = data[features]
-        y = data[args]
+        # if not args:
+        y = data['next_direction']
 
         # normalization
         X = preprocessing.scale(X)
@@ -25,25 +27,19 @@ class LinearRegressionModel(BaseModel):
         pca = PCAModel(self.module_name).load(code)
         X = pca.transform(X)
 
-        X_train, x_test, y_train, y_test = train_test_split(X, y, test_size=.3, shuffle=False)
+        X_train, x_test, y_train, y_test = train_test_split(X, y, test_size=.3, shuffle=False, random_state=10)
 
-        LR_model = linear_model.LinearRegression(n_jobs=-1)
-
-        logger.debug(LR_model)
+        LR_model = linear_model.LinearRegression(n_jobs=-1, normalize=True)
 
         LR_model.fit(X_train, y_train)
 
-        y_pred = cross_val_score(LR_model, x_test, n_jobs=-1)
+        y_pred = LR_model.predict(x_test)
 
-        test_score = accuracy_score(y_test, y_pred)
-
-        logger.debug('test score %s ' %test_score)
+        logger.debug('mse: %s' % metrics.mean_squared_error(y_test, y_pred))
 
         LR_model.fit(X, y)
 
-        full_set_score = LR_model.score(X, y)
 
-        logger.debug('full set score %s' % full_set_score)
 
     def predict(self, code, data):
         pass
