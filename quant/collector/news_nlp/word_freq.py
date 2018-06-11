1664a3fc60113d5a554e2c650c1feba0dd8eb23f
aedotpy <aedotpy@gmail.com>
2018-06-11 19:57:12 +0800

    stop words

--- quant/collector/news_nlp/word_freq.py
+++ quant/collector/news_nlp/word_freq.py
@@ -3,48 +3,90 @@
 
 import nltk
 import tushare as ts
-
-instant_news_df = ts.get_latest_news(show_content=True)
-sina_new_df = ts.guba_sina(show_content=True)
-
-
-instant_titles = instant_news_df['title']
-instant_content = instant_news_df['content']
-
-sina_titles = sina_new_df['title']
-sina_content = sina_new_df['content']
+import jieba.posseg as pseg
+import pandas as pd
+from nltk.corpus import stopwords
 
 
-def word_an
+def muning_tushare_new():
+    instant_news_df = ts.get_latest_news(show_content=True)
+    sina_new_df = ts.guba_sina(show_content=True)
+    instant_titles = instant_news_df['title']
+    instant_content = instant_news_df['content']
 
-instant_title_str = ''
-for i_t in instant_titles:
-    if i_t is None:
-        it = ''
-    instant_title_str = instant_title_str + i_t
+    sina_titles = sina_new_df['title']
+    sina_content = sina_new_df['content']
 
-instant_content_str = ''
-for i_c in instant_content:
-    if i_c is None:
-        i_c = ''
-    instant_content_str = instant_content_str + i_c
+    instant_title_str = ''
+    for i_t in instant_titles:
+        if i_t is None:
+            it = ''
+        instant_title_str = instant_title_str + i_t
 
-sina_title_str = ''
-for s_t in sina_titles:
-    if s_t is None:
-        s_t = ''
-    sina_title_str = sina_title_str + s_t
+    instant_content_str = ''
+    for i_c in instant_content:
+        if i_c is None:
+            i_c = ''
+        instant_content_str = instant_content_str + i_c
 
-sina_content_str = ''
-for s_c in sina_content:
-    if s_c is None:
-        s_c = ''
-    sina_content_str = sina_content_str + s_c
+    sina_title_str = ''
+    for s_t in sina_titles:
+        if s_t is None:
+            s_t = ''
+        sina_title_str = sina_title_str + s_t
 
-new_collection = instant_title_str + instant_content_str + sina_title_str + sina_content_str
-print(new_collection)
+    sina_content_str = ''
+    for s_c in sina_content:
+        if s_c is None:
+            s_c = ''
+        sina_content_str = sina_content_str + s_c
+
+    news_collection = instant_title_str + instant_content_str + sina_title_str + sina_content_str
+
+    return news_collection
 
 
+def word_analysis(args):
+    # stops_words = set(stopwords.words("english"))
+
+    stops_words = []
+
+    with open('/Users/yw.h/quant-awesome/quant/collector/news_nlp/stop_word.txt', 'rb') as f:
+        for line in f:
+
+            line = line.decode('utf-8').strip('\n')
+            stops_words.append(line)
+
+    w_list = []
+
+    words = pseg.cut(args)
+
+    for keys in words:
+        w_list.append(keys.word)
+
+    print(stops_words)
+
+    print(w_list)
+    filtered_words = []
+
+    for word in w_list:
+        if word not in stops_words:
+            filtered_words.append(word)
+        else:
+            pass
+    print(filtered_words)
+
+    # freq_result = nltk.FreqDist(filtered_words)
+    #
+    # freq_dict = {}
+    # for i in freq_result:
+    #     freq_dict[i] = freq_result[i]
+    #
+    # sorted_list = sorted(freq_dict.items(), key=lambda item: item[1], reverse=True)
+    #
+    # print(sorted_list)
 
 
-
+if __name__ == '__main__':
+    sth = muning_tushare_new()
+    word_analysis(sth)
