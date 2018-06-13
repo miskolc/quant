f18dac6537ac43a58dd3bf087c57a7a880080bd0
aedotpy <aedotpy@gmail.com>
2018-06-13 14:26:59 +0800

    no message

--- quant/feature_utils/feature_collector.py
+++ quant/feature_utils/feature_collector.py
@@ -25,7 +25,7 @@ price_list = [p for p in getmembers(price_transform) if isfunction(p[1])]
 volatility_list = [l for l in getmembers(volatility_indicators) if isfunction(l[1])]
 custome_list = [u for u in getmembers(custome_features) if isfunction(u[1])]
 stistic_list = [s for s in getmembers(statistic_functions) if isfunction(s[1])]
-#pattern_list = [r for r in getmembers(pattern_recognition) if isfunction(r[1])]
+# pattern_list = [r for r in getmembers(pattern_recognition) if isfunction(r[1])]
 
 func_list = momentum_list + overlaps_list + volume_list + volume_list + cycle_list + price_list + volatility_list + custome_list + stistic_list
 
@@ -44,9 +44,9 @@ def collect_features(df):
     for func in func_list:
         if hasattr(momentum_indicators, func[0]) or hasattr(overlaps_studies, func[0]) or hasattr(volume_indicators,
                                                                                                   func[0]) or hasattr(
-                cycle_indicators, func[0]) or hasattr(price_transform, func[0]) or hasattr(volatility_indicators,
-                                                                                           func[0]) or hasattr(
-                custome_features, func[0]) or hasattr(statistic_functions, func[0]):
+            cycle_indicators, func[0]) or hasattr(price_transform, func[0]) or hasattr(volatility_indicators,
+                                                                                       func[0]) or hasattr(
+            custome_features, func[0]) or hasattr(statistic_functions, func[0]):
             col_name = re.match(r'(^.{4})(.{0,})', func[0]).group(2)
             func_feature = func[1](df)
 
@@ -58,7 +58,6 @@ def collect_features(df):
                 col_list.append(col_name)
             else:
                 df = df.join(func_feature)
-                # col_list.append(list(func_feature.columns.values))
                 col_names_list = func_feature.columns.values
                 for col in col_names_list:
                     col_list.append(col)
@@ -68,25 +67,3 @@ def collect_features(df):
     col_list = list(set(col_list))
     return df, col_list
 
-
-# big_list = os.listdir(root_path)
-# # # print(big_list)
-# big_list.remove('__init__.py')
-# big_list.remove('__pycache__')
-# big_list.remove('feature_collector.py')
-# # print(big_list)
-#
-#
-# module_list = []
-# for module in big_list:
-# #     # print(module)
-#     print(__import__(module))
-#     # print(getmembers(dir_path+module))
-#     # print([m for m in getmembers(dir_path+module) if isfunction(m[1])])
-#     # module_list.append([m for m in getmembers(module) if isfunction(m[1])])
-#
-# # print(module_list)
-
-
-# if __name__ == '__main__':
-#     get_col_name_list(pattern_list)
