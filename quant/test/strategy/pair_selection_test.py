60225191614b42b8ec5857cc96a622059916ea7c
aedotpy <aedotpy@gmail.com>
2018-07-05 16:16:35 +0800

    no message

--- quant/test/strategy/pair_selection_test.py
+++ quant/test/strategy/pair_selection_test.py
@@ -1,11 +1,19 @@
 # coding =utf-8
 # ae_h - 2018/6/4
+import os
+import sys
+
+CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
+ROOT_DIR = os.path.dirname(os.path.dirname(CURRENT_DIR))
+sys.path.append(ROOT_DIR)
+
 import unittest
 
 from quant.test import before_run
 from quant.strategy.pair_selection import code_muning, cal_p_value, cal_pair_stocks
 import tushare as ts
-from quant.dao.k_data.k_data_dao import k_data_dao
+
+
 
 
 class PairStrategyTest(unittest.TestCase):
