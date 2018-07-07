6bf718470609e7ef34e79ec12630ce64ab8de368
aedotpy <aedotpy@gmail.com>
2018-07-07 19:35:34 +0800

    no message

--- quant/common_tools/decorators.py
+++ quant/common_tools/decorators.py
@@ -11,7 +11,7 @@ def exc_time(func):
         start_time = time.time()
         tmp = func(*args, **kv)
         end_time = time.time()
-        logger.debug("%s executed,  elapsed time: %.2f ms" % (func.__name__, (end_time - start_time) * 1000))
+        logger.debug("%s executed,  elapsed time: %.2f ms / %s" % (func.__name__, (end_time - start_time) * 1000, int(end_time - start_time)))
         return tmp
 
     return fn
---
+++ quant/test/strategy/kdj_test.py
@@ -0,0 +1,20 @@
+# ae_h - 2018/7/7
+
+import numpy as np
+from quant.dao.k_data.k_data_dao import k_data_dao
+from quant.feature_utils.momentum_indicators import acc_kdj
+import pandas as pd
+from quant.common_tools.decorators import exc_time
+
+@exc_time
+def cal_kdj():
+
+    df = k_data_dao.get_k_data_all()
+
+    k_d_j = acc_kdj(df)
+
+    df_with_kdj = pd.concat([df, k_d_j], axis=1)
+
+    print(df_with_kdj)
+
+
