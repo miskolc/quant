fad68a9f27464769e08e4cdda4fdf9b04d541691
aedotpy <aedotpy@gmail.com>
2018-07-12 22:00:57 +0800

    no message

---
+++ futu_test.py
@@ -0,0 +1,8 @@
+# ae_h - 2018/7/12
+import futuquant as ft
+
+
+quote_ctx = ft.OpenQuoteContext(host='192.168.80.32', port=11111)
+state, data = quote_ctx.get_history_kline('SH.600196', start='2010-01-01', end='2018-07-11', ktype='K_DAY', autype='qfq')
+
+print(data)
