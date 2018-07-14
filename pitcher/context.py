ae9b1de65348a44e270a1725bffc757a369376e9
aedotpy <aedotpy@gmail.com>
2018-07-14 17:09:42 +0800

    no message

--- pitcher/context.py
+++ pitcher/context.py
@@ -8,10 +8,12 @@ class Context:
         self.start = start
         self.end = end
         self.base_capital = base_capital
-        self.profit_df = pd.DataFrame(columns=[ ])
-        self.protofolio_df = pd.DataFrame(columns=[??])
-        self.order_book_df = pd.DataFrame(columns=[??])
-
+        self.profit_df = pd.DataFrame(
+            columns=['stock_code', 'daily_profit', 'daily_change_rate', 'total_profit', 'total_change_rate'])
+        self.order_book_df = pd.DataFrame(
+            columns=['stock_code', 'action', 'shares', 'price_in', 'price_out', 'date_time'])
+        self.protofolio_df = pd.DataFrame(
+            columns=['stock_code', 'shares', 'price'])
 
     def __getattr__(self, item):
         return item
