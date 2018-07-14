8998bb8eff171a080ab26411c07c2e076fc1f0a2
aedotpy <aedotpy@gmail.com>
2018-07-14 16:57:14 +0800

    no message

--- judger/manage.py
+++ judger/manage.py
@@ -1,8 +1,9 @@
 # ae_h - 2018/7/13
 from judger.back_test_judger import DailyBackTestJudger
-from pitcher.kdj_st import kdj_strategy
+from pitcher.kdj_st import KDJStrategy
 
 # daily
+kdj_strategy = KDJStrategy()
 daily_bt = DailyBackTestJudger(start='2017-01-01', end='2018-07-01', base_capital=50000)
 
 daily_bt.execute(kdj_strategy)
--- pitcher/context.py
+++ pitcher/context.py
@@ -1,11 +1,17 @@
 # ae_h - 2018/7/13
 
-class Context:
+import pandas as pd
 
+
+class Context:
     def __init__(self, start, end, base_capital):
         self.start = start
         self.end = end
         self.base_capital = base_capital
+        self.profit_df = pd.DataFrame(columns=[ ])
+        self.protofolio_df = pd.DataFrame(columns=[??])
+        self.order_book_df = pd.DataFrame(columns=[??])
+
 
     def __getattr__(self, item):
         return item
