1c7b9e3e4d846b8e07d376a2669541175dadcbcb
aedotpy <aedotpy@gmail.com>
2018-07-15 17:01:15 +0800

    no message

--- pitcher/kdj_st.py
+++ pitcher/kdj_st.py
@@ -12,6 +12,7 @@ import pandas as pd
 from feature_utils.overlaps_studies import cal_ma5, cal_ma10, cal_ma20, cal_ma60
 from feature_utils.custome_features import cal_mavol5, cal_mavol10, cal_mavol20
 from dao.basic.stock_pool_dao import stock_pool_dao
+from dao.basic.stock_basic_dao import stock_basic_dao
 
 class KDJStrategy:
 
@@ -23,21 +24,30 @@ class KDJStrategy:
         # for code in context.pool:
         daily_stock_data = k_data_dao.get_k_data(code='SZ.000528', start=get_next_date(days=-30, args=context.current_date),
                                            end=get_current_date(context.current_date))
+        weekly_stock_date = 
+        daily_stock_data = daily_stock_data.join(acc_kdj(daily_stock_data))
 
-        # stock_data = stock_data.join(acc_kdj(stock_data))
-        ma_frame = pd.concat([acc_kdj(daily_stock_data), cal_ma5(daily_stock_data), cal_ma10(daily_stock_data), cal_ma20(daily_stock_data), cal_ma60(daily_stock_data),
-                              cal_mavol5(daily_stock_data), cal_mavol10(daily_stock_data), cal_mavol20(daily_stock_data)], axis=1)
+        # stock_data = daily_stock_data.join(ma_frame)
+        k_value = daily_stock_data['k_value'].iloc[-1:].values[0]
+        d_value = daily_stock_data['d_value'].iloc[-1:].values[0]
+        pre_k = daily_stock_data['k_value'].iloc[-2:].values[0]
+        pre_d = daily_stock_data['d_value'].iloc[-2:].values[0]
 
-        ma_frame.columns = ['k_value', 'd_value', 'j_value', 'ma5', 'ma10', 'ma20', 'ma60', 'mavol5', 'mavol10', 'mavol20']
-        stock_data = daily_stock_data.join(ma_frame)
-        k_value = ma_frame['k_value'].iloc[-1:].values[0]
-        d_value = ma_frame['d_value'].iloc[-1:].values[0]
-        pre_k = ma_frame['k_value'].iloc[-2:].values[0]
-        pre_d = ma_frame['d_value'].iloc[-2:].values[0]
+        # 金叉
+        if pre_k < pre_d and ((k_value >= d_value) or (abs(k_value - d_value)<=5)):
+            feature_frame = pd.concat([acc_kdj(daily_stock_data), cal_ma5(daily_stock_data), cal_ma10(daily_stock_data),
+                                       cal_ma20(daily_stock_data), cal_ma60(daily_stock_data),
+                                       cal_mavol5(daily_stock_data), cal_mavol10(daily_stock_data),
+                                       cal_mavol20(daily_stock_data)], axis=1)
 
-        print((k_value, d_value), (pre_k, pre_d))
+            feature_frame.columns = ['k_value', 'd_value', 'j_value', 'ma5', 'ma10', 'ma20', 'ma60', 'mavol5',
+                                     'mavol10', 'mavol20']
+            basic_data = stock_basic_dao.get_by_code(code='000528')
 
-        print(stock_data)
+        # 死叉
+        if pre_k > pre_d and ((k_value <= d_value) or (abs(k_value - d_value)<=5)):
+
+
 
 
 if __name__ == '__main__':
