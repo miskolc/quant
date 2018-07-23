f60174b27c56368c16bcf45a80b3b8c1a8c41107
greg <czchen228@gmail.com>
2018-07-23 11:14:37 +0800

    k_data_weekly_dao

--- test/dao/k_data_week/k_data_week_dao.py
+++ test/dao/k_data_week/k_data_week_dao.py
@@ -1,16 +1,22 @@
 import unittest
 
+from config import default_config
 from dao.k_data_weekly.k_data_weekly_dao import k_data_weekly_dao
-from test import before_run
-
+import futuquant as ft
 
 class K_Data_Week_Dao_Test(unittest.TestCase):
 
     def setUp(self):
-        before_run()
+
+        self.futu_quote_ctx = ft.OpenQuoteContext(host=default_config.FUTU_OPEND_HOST,
+                                                  port=default_config.FUTU_OPEND_PORT)
+
+
+    def tearDown(self):
+        self.futu_quote_ctx.close()
 
     def test_get_k_data(self):
-        df = k_data_weekly_dao.get_k_data('SH.600196', '2018-05-01','2018-07-06')
+        df = k_data_weekly_dao.get_k_data('600196',start=None, end=None, futu_quote_ctx=self.futu_quote_ctx)
 
         print(df)
 
