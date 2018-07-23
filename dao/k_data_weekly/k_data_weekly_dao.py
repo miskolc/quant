5a33b6cdf538644a244e9169ba96d1c46c0f84cf
greg <czchen228@gmail.com>
2018-07-23 17:55:22 +0800

    军工板块策略

--- dao/k_data_weekly/k_data_weekly_dao.py
+++ dao/k_data_weekly/k_data_weekly_dao.py
@@ -14,6 +14,23 @@ class K_Data_Weekly_Dao:
 
         return data
 
+    @exc_time
+    def get_multiple_history_kline(self, code_list, start, end, futu_quote_ctx):
+        code_list = list(map(fill_market, code_list))
+
+        state, data = futu_quote_ctx.get_multiple_history_kline(codelist=code_list
+                                                                , start=start, end=end,  ktype='K_WEEK', autype='qfq')
+
+        k_data_dict = {}
+        for item in data:
+            if item is None or len(item["code"]) <=0:
+                continue
+
+            code = item["code"].tail(1).values[0]
+            k_data_dict[code] = item
+
+        return k_data_dict
+
     '''
     @exc_time
     def get_k_data_all(self):
