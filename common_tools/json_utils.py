ee9ec2145b0ca91718fd8f3466c8d54b49fd9c69
greg <czchen228@gmail.com>
2018-07-25 19:20:07 +0800

    no message

--- common_tools/json_utils.py
+++ common_tools/json_utils.py
@@ -1,4 +1,12 @@
+import numpy as np
+import json
+from datetime import date, datetime
 
+class JSONEncoder(json.JSONEncoder):
+    """ Special json encoder for numpy types """
 
-def obj_dict(obj):
-    return obj.__dict__
+    def default(self, obj):
+        if isinstance(obj, (datetime, date)):
+            return obj.isoformat()
+
+        return obj.__dict__
