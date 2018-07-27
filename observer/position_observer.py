9e43d6d2052289e80d0b481bca461ee79b7c54e1
greg <czchen228@gmail.com>
2018-07-27 12:01:23 +0800

    observer

--- observer/position_observer.py
+++ observer/position_observer.py
@@ -2,29 +2,30 @@
 import os
 import sys
 
-# Append project path to system path
+
 CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
 ROOT_DIR = os.path.dirname(CURRENT_DIR)
 sys.path.append(ROOT_DIR)
 
-
-from apscheduler.schedulers.blocking import BlockingScheduler
-
-from config import default_config
+# Append project path to system path
 from dao.k_data import fill_market
 from dao.trade.position_dao import position_dao
+
+from apscheduler.schedulers.blocking import BlockingScheduler
 from datetime import datetime
+from config import default_config
+
 import futuquant as ft
+
+futu_quote_ctx = ft.OpenQuoteContext(host=default_config.FUTU_OPEND_HOST, port=default_config.FUTU_OPEND_PORT)
 
 
 def monitor():
     try:
+
         positions = position_dao.get_position_list()
 
         codes = [fill_market(p.code) for p in positions]
-
-        futu_quote_ctx = ft.OpenQuoteContext(host=default_config.FUTU_OPEND_HOST,
-                                             port=default_config.FUTU_OPEND_PORT)
 
         state, df = futu_quote_ctx.get_market_snapshot(codes)
         # 更新实时数据
@@ -39,10 +40,14 @@ def monitor():
             position.profit = profit
             position.update_time = datetime.now()
             position_dao.update(position)
+
     finally:
-        futu_quote_ctx.close()
+        pass
+
 
 if __name__ == '__main__':
-    scheduler = BlockingScheduler()
-    scheduler.add_job(monitor, 'cron', day_of_week='1-5', hour='9-15',second='*/15')
-    scheduler.start()
+
+     scheduler = BlockingScheduler()
+     scheduler.add_job(monitor, 'cron', day_of_week='1-5', hour='9-15',second='*/10')
+     scheduler.start()
+
